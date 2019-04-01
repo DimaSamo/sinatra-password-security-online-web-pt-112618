766287882eb5d4f856b54272d6b58e4c00e07e67
Dmitri Samotessov <dimasamo96@gmail.com>
2019-04-01 06:07:18 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,6 +26,11 @@ class ApplicationController < Sinatra::Base
 
 	post "/login" do
 		@user=User.new(username: params[:username], password: params[:password])
+		if user.save
+			redirect "/login"
+		else
+			redirect "/failure"
+		end
 	end
 
 	get "/success" do
